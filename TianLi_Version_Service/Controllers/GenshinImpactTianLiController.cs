4e9db25644b28974e5e86ea9b740767e765bb44f
GengGode <GengGode@outlook.com>
2022-10-24 10:18:46 +0800

    添加项目文件。

---
+++ TianLi_Version_Service/Controllers/GenshinImpactTianLiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TianLi_Version_Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GenshinImpactTianLiController : ControllerBase
+    {
+        private readonly ILogger<GenshinImpactTianLiController> _logger;
+
+        public GenshinImpactTianLiController(ILogger<GenshinImpactTianLiController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        [Route("Version/Latest")]
+        public IActionResult GetLatestVersion()
+        {
+            return Ok(new GenshinImpactTianLi());
+        }
+    }
+}
---
+++ TianLi_Version_Service/GenshinImpactTianLi.cs
@@ -0,0 +1,10 @@
+﻿namespace TianLi_Version_Service
+{
+    public class GenshinImpactTianLi
+    {
+        public string Version => "0.0.0";
+
+        public string DownloadLink => "GenshinImpact_TianLi/Download/0.0.0";
+
+    }
+}
